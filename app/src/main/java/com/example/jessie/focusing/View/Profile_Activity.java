c44e5edd4bcba0f7b4705b1f1a45832642b0dbf5
YujieLyu <lyuy4@student.unimelb.edu.au>
2019-01-23 01:18:16 +1100

    Add profile UI

--- app/src/main/java/com/example/jessie/focusing/View/Profile_Activity.java
+++ app/src/main/java/com/example/jessie/focusing/View/Profile_Activity.java
@@ -1,9 +1,51 @@
 package com.example.jessie.focusing.View;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.jessie.focusing.R;
+
 /**
  * @author : Yujie Lyu
  * @date : 19-01-2019
  * @time : 01:02
  */
-public class Profile_Activity {
+public class Profile_Activity extends AppCompatActivity {
+    private TextView tv_suggestion;
+    private Button btn_p1, btn_new;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        tv_suggestion=findViewById(R.id.tv_suggest_profile);
+        btn_p1=findViewById(R.id.btn_p1);
+        btn_new=findViewById(R.id.btn_new);
+        btn_p1.setBackgroundColor(Color.argb(70, 255, 255, 255)); //背景透明度
+        btn_new.setBackgroundColor(Color.argb(70, 255, 255, 255));
+
+        setStatusTransparent();
+
+    }
+    protected void setStatusTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // 5.0+ 实现
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 4.4 实现
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
 }
