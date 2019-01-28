5ee70bc11f0ff52fabf6f1a1c7e08c76b6f42ba4
YujieLyu <lyuy4@student.unimelb.edu.au>
2019-01-28 19:07:16 +1100

    Show today's schedule done

---
+++ app/src/main/java/com/example/jessie/focusing/Controller/Adapter/ScheduleListAdapter.java
@@ -0,0 +1,112 @@
+package com.example.jessie.focusing.Controller.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.jessie.focusing.Model.Profile;
+import com.example.jessie.focusing.Model.ProfileManager;
+import com.example.jessie.focusing.R;
+import com.example.jessie.focusing.View.Profile.ProfileDetailActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Yujie Lyu
+ * @date : 28-01-2019
+ * @time : 04:06
+ */
+public class ScheduleListAdapter extends BaseAdapter {
+
+    private List<Profile> profiles = new ArrayList<>();
+    private Context context;
+
+    private ProfileManager profileManager;
+    private FragmentManager fragmentManager;
+
+    public ScheduleListAdapter(Context context) {
+        this.context = context;
+        profileManager = new ProfileManager(context);
+    }
+
+    public void setData(int today) {
+//        Profile profile1=new Profile("Meeting");
+        profiles=profileManager.syncProfileOnSchedule(today);
+
+        notifyDataSetChanged();
+    }
+
+    public void addSchedule(Profile profile){
+        //todo:调用Profdetail设置页面，将结果返回给schedule
+//        profileManager.updateProfile(profile);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ScheduleListAdapter.ViewHolder viewHolder = new ScheduleListAdapter.ViewHolder();
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_schedule_list, null);
+        }
+        viewHolder.profileName = convertView.findViewById(R.id.profile_name);
+        viewHolder.timeSlot = convertView.findViewById(R.id.timeSlot);
+        viewHolder.repeat = convertView.findViewById(R.id.repeat);
+        if (profiles.size()==0){
+            viewHolder.profileName.setText("Today has no schedule");
+        }else {
+            Profile selectedInfo = profiles.get(position);
+
+
+            viewHolder.profileName.setText(selectedInfo.getProfileName());
+            String timeSlot = selectedInfo.getStartHour() + ":" + selectedInfo.getStartMin()
+                    + "~" + selectedInfo.getEndHour() + ":" + selectedInfo.getEndMin();
+            viewHolder.timeSlot.setText(timeSlot);
+            viewHolder.repeat.setText(selectedInfo.getRepeat());
+        }
+//        viewHolder.tv_ProfName.setOnClickListener(this);
+        return convertView;
+    }
+
+
+    
+
+    //todo:class和Class区别？
+    class ViewHolder {
+
+        TextView profileName,timeSlot,repeat;
+
+
+
+    }
+//    public void syncData() {
+//        profileManager.syncData(appInfos);
+//    }
+
+    @Override
+    public int getCount() {
+        if (profiles != null && profiles.size() > 0) {
+            return profiles.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (profiles != null && profiles.size() > 0) {
+            return profiles.get(position);
+        }
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+}
